28e199bcedf82645d9678a5f9fe364aaa6986b22
aryan18-08 <aryxn.sharma18@gmail.com>
2025-01-19 13:14:13 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,99 @@
+# #Q1
+# print("Anything you find cool")
+
+# #Q2.1
+# a=12
+# b=24
+# print(a+b)
+
+# #Q2.2
+# a="Pine"
+# b="apple"
+# print(a+b)
+
+# #Q2.3
+# c=6
+# d=" monkeys"
+# print(str(c)+d)
+
+# #Q3.1
+# x=int(input("Enter the number: "))
+# if(x>0):
+#     print("number is positive")
+# elif(x<0):
+#     print("number is negative")
+# else:
+#     print("number is zero")
+
+# #Q3.2
+# y=int(input("Enter the number: "))
+# if(y%2==0):
+#     print("Number is even")
+# else:
+#     print("number is odd")
+
+#Q4.1
+# for i in range(1,11):
+#     print(i)
+
+# #Q4.2
+# count=1
+# while(count<11):
+#     print(count)
+#     count+=1
+
+# #Q4.3
+# sum=0
+# for i in range(1,101):
+#     sum+=i
+# print(sum)
+
+# Q5.1
+# list1=[]
+# print("Enter the numbers of the list: ")
+# for i in range(0,5):
+#     a=int(input())
+#     list1.append(a)
+# print(f"The minimum number is: {min(list1)}")
+# print(f"The maximum number is: {max(list1)}")
+
+#Q5.2
+# dict={1:"english",2:"hindi",3:"maths"}
+# print(dict)
+# print(dict[1])
+
+#Q5.3
+# x=[2,6,3,9,4,1]
+# x.sort()
+# print(x)
+# x.sort(reverse=True)
+# print(x)
+
+#Q5.4
+# d1={'k1':"first",'k2':"second",'k3':"third"}
+# d2={'k4':"fourth",'k5':"fifth",'k6':"sixth"}
+# d3=d1|d2
+# print(d3)
+
+#Q6.1
+# x=input("Enter the string: ")
+# count=0
+# for i in range(len(x)):
+#     if(x[i]=='a' or x[i]=='e' or x[i]=='i' or x[i]=='o' or x[i]=='u'):
+#         count+=1
+# print(f"The total number of vowels in this string is {count}")
+
+#Q6.2
+# x=input("Enter the string: ")
+# y=x[::-1]
+# print(y)
+
+#Q6.3
+# a=input("Enter the string: ")
+# b=a[::-1]
+# if(a==b):
+#     print("the given string is a palindrome")
+# else:
+#     print("not a palindrome")
+
+#Q7.1
